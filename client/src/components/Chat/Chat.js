e74b51b7d77f7146ae8e5536460ad5af11d1813b
jamestang12 <homing.tang@ryerson.ca>
2020-03-19 21:13:11 -0400

    Third Commit

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Chat.css';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        socket.emit('join', { name, room }, () => { //join is the function name
+
+        });
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [ENDPOINT], location.search)
     return (
 
-        <h1>Chat</h1>    
+        <h1>Chat</h1>
     )
 
 }
--- server/index.js
+++ server/index.js
@@ -6,11 +6,30 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const { addUser, removeUser, getUser, getUsersInRomm } = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
 
 io.on('connection', (socket) => {
-    console.log('we have a new connection');
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({id: socket.id, name, room});
+        if (error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, Welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined` }); //broadcast beside that user
+
+        socket.join(user.room);
+
+        callback(); //Error hadnler that send error message 
+
+    });
+
+    socket.on('sendMessage', (message, callback) => { //socket.on take in two things (keystring , arrow function)
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        callback();
+    }) 
 
     socket.on('disconnect', () => {
         console.log('User had left');
